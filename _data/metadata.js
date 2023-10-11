c08624b480030fe72329d88cbf375ca8dd86ee4c
Ellen Furnell <ellenfurnell82@yahoo.com>
2023-10-11 13:21:45 +0100

    added metadata, removed message from home layout
    added metadata, removed message from home layout

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Ellen's Blog",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my experiences.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Ellen Furnell",
+		email: "ellenfurnell82@yahoo.com",
 		url: "https://example.com/about-me/"
 	}
 }
